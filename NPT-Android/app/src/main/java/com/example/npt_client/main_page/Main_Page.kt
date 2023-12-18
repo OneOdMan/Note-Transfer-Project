62783851f67fcf08c768e3a485a9e4dcce05a2d3
OneOdMan <namdo2000@gmail.com>
2023-12-17 17:48:24 -0800

    --[Initial Commit]-- The intial commit of the repo.  Adding the API and Android Project sources.

---
+++ NPT-Android/app/src/main/java/com/example/npt_client/main_page/Main_Page.kt
@@ -0,0 +1,78 @@
+package com.example.npt_client.main_page
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.npt_client.R
+import com.example.npt_client.ui.theme.NPT_ClientTheme
+
+class Main_Page : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NPT_ClientTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainPageTopBar()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainPageTopBar() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(stringResource(id = R.string.app_name))
+                }
+            )
+        },
+    ) { innerPadding ->
+        ScrollContent(innerPadding)
+    }
+}
+
+@Composable
+fun ScrollContent(innerPadding: PaddingValues) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    NPT_ClientTheme {
+        Greeting2("Android")
+    }
+}
---
+++ NPT-Android/app/src/main/java/com/example/npt_client/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.npt_client.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PaleTeal = Color(0xFF84F1FF)
+val DarkerTeal = Color(0xFF369AA7)
+val Teal = Color(0xFF03E3FF)
+
+val LightGreen = Color(0xFF2BFFCE)
+val DarkGreen = Color(0xFF1C887B)
+val Green = Color(0xFF00D498)
