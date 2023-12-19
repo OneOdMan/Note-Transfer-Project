6b79cbc2eece88b03e790f614cc1bdc4f9bd4ffb
OneOdMan <namdo2000@gmail.com>
2023-12-18 21:19:16 -0800

    --[Main Modal: Fixed Bug]-- - Fixed the bug where modal appears before clicking on plugin icon New Bug: Button does not show up

--- Test-Vault/.obsidian/plugins/ntp-plugin/main.ts
+++ Test-Vault/.obsidian/plugins/ntp-plugin/main.ts
@@ -20,7 +20,7 @@ export default class MyPlugin extends Plugin {
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new MainModal(this.app).open();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
