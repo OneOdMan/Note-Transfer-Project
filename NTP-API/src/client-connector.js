1047e969accf7882eff69dcefce6a802cf6c12a8
OneOdMan <namdo2000@gmail.com>
2023-12-22 08:51:57 -0800

    --[NTP API and Server]-- Continuing to initiate the Server and API

---
+++ NTP-API/src/client-connector.js
@@ -0,0 +1,6 @@
+// Waiting for a connect request from client and processes the type of request
+// Sends connect message to the Server and waits for a response 
+// Send Data Package Request back to the Client if Server needs it
+// Sends Package received message back to client
+// Sends sync complete message to both server and client once the whole process is done
+// Sends error messages if anything comes up
---
+++ NTP-API/src/packageManager.js
@@ -0,0 +1,5 @@
+// Send packages to the Server and wait for a response
+// Ask Client for Data if the Server asks for it using client connector
+// Send Client Data to the Server
+// Send Data to the Client to download if neccessary
+// Send message to the Client confirming sync completion
