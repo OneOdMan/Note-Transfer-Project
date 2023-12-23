5805803c44918bcef88596367014edd2ed09aefc
OneOdMan <namdo2000@gmail.com>
2023-12-22 21:57:23 -0800

    --[NTP API | Routing and Initializing Function]-- Messing around more with routing and initializing Client Connection Function
    - Seeing how to create routes and setting the function to listen on port
      3000
    - Will focus on initializing the Server for now to get a better idea of
      how sending data will work

--- NTP-API/index.js
+++ NTP-API/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const bodyParser=require("body-parser");
 
 const {CCRouter} = require('./src/ClientConnector/CC_Routes');
+
 //Initalizing and routing the Client Connector
 const ccFunc = express();
 ccFunc.use(bodyParser.json());
 ccFunc.use(bodyParser.urlencoded({extended:false}));
 ccFunc.use(CCRouter);
+
+// Running on Local Host Docker container
+ccFunc.listen(3000, () => {
+    console.log('ClientConnector Function listening on port 3000');
+});
