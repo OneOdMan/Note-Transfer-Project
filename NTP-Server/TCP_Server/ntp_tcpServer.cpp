5510c14af4d2916fe1daccedba893c0b9e0a184a
OneOdMan <namdo2000@gmail.com>
2023-12-23 09:34:28 -0800

    --[NTP Server | Initialize Listening and Connecting]-- Initialized components needed listen for connection requests and accept connections
    - Added a function to listen for connection requests in startListen()
    - Added a function to accept a connect request in acceptConnection()
    - Added the components for the above functions in the h file

--- NTP-Server/TCP_Server/ntp_tcpServer.cpp
+++ NTP-Server/TCP_Server/ntp_tcpServer.cpp
@@ -14,18 +14,30 @@ namespace{
     }
 }
 
+
 namespace http{
     Ntp_TcpServer::Ntp_TcpServer(std::string ip_address, int port)
         :   m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(), m_incomingMessage(),
             m_socketAddress(), m_socketAddress_len(sizeof(m_socketAddress)), m_serverMessage(buildResponse())
     {
+        m_socketAddress.sin_family = AF_INET;
+        m_socketAddress.sin_port = htons(m_port);
+        m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str());
     }
     Ntp_TcpServer::~Ntp_TcpServer(){
         closeServer();
     }
 
+//-------------[Running the Server]----------------
     int Ntp_TcpServer::startServer(){
         m_socket = socket(AF_INET, SOCK_STREAM, 0);
+        
+        // Binding the socket address to the given socket
+        if( bind(m_socket,(sockaddr *)&m_socketAddress, m_socketAddress_len) < 0 ){
+            exitWithError("Cannot connect socket to address");
+            return 1;
+        }
+
         if (m_socket < 0)
         {
             exitWithError("Cannot create socket");
@@ -37,5 +49,28 @@ namespace http{
         close(m_socket);
         close(m_new_socket);
         exit(0);
+    }
+//-------------[Connecting to the Server]----------------
+    // Listening for incomming connections
+    void Ntp_TcpServer::startListen(){
+        if(listen(m_socket, 20) < 0)
+            exitWithError("Socket listen failed");
+        std::ostringstream ss;
+        ss  << "\n*** Listening on Address: "
+            << inet_ntoa(m_socketAddress.sin_addr)
+            << " Port: " << ntohs(m_socketAddress.sin_port)
+            << " ***\n\n";
+        log(ss.str());
+    }
+    //Accepting the connection
+    void Ntp_TcpServer::acceptConnection(SOCKET &new_socket){
+        new_socket = accept(m_socket, (sockaddr *)&m_socketAddress, &m_socketAddress_len);
+        if(new_socket < 0){
+            std::ostringstream ss;
+            ss  << "Server failed to accept incoming connection from Address: "
+                << inet_ntoa(m_socketAddress.sin_addr) << "; PORT: "
+                << ntohs(m_socketAddress.sin_port);
+            exitWithError(ss.str());
+        }
     }
 }
--- NTP-Server/TCP_Server/ntp_tcpServer.h
+++ NTP-Server/TCP_Server/ntp_tcpServer.h
@@ -7,6 +7,17 @@
 #include <stdlib.h>
 #include <string>
 
+struct sockaddr_in{
+    short           sin_family;     // Socket Family. Using AF_INET
+    unsigned short  sin_port;       // Server Port
+    struct in_addr  sin_addr;       // Server IP Address
+    char            sin_zero[8];    // Buffer to adjust size of sockaddr_in
+};
+
+struct in_addr {
+    unsigned long s_addr;
+};
+
 namespace http{
     class Ntp_TcpServer{
         public:
@@ -23,8 +34,10 @@ namespace http{
             unsigned int m_socketAddress_len;
             std::string m_serverMessage;
 
-            int startServer();
+            int  startServer();
             void closeServer();
+            void startListen();
+            void acceptConnection(SOCKET &new_socket);
     };
 }
 #endif
